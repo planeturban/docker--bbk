4501c462e14dfd3877f15f284829fc145dd72962
planeturban <ube@nohost>
2019-04-19 10:12:00 +0000

    First commit

---
+++ scripts/init_test_connection.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+FILE="/app/bbk.log"
+
+if [ -z $INFLUX_HOST ]
+then
+	export INFLUX_HOST=influxdb
+fi
+if [ -z $TEST_INTERVAL ]
+then
+	export TEST_INTERVAL=300
+fi
+
+echo Influx host:   $INFLUX_HOST
+echo Test interval: $TEST_INTERVAL
+
+UPLOAD="-1"
+while true 
+do 
+
+	while [ "$UPLOAD" == "-1" ]
+	do
+		/app/bbk --quiet --server=$(/app/bbk --check-servers |grep -v Network| awk '{print $3}' | shuf -n 1) > $FILE
+		TIMESTAMP=$(date '+%s')
+		UPLOAD=$(awk '{print $3}' $FILE)
+		PING=$(awk '{print $1}' $FILE)
+		DOWNLOAD=$(awk '{print $2}' $FILE)
+	done
+	echo "Download: $DOWNLOAD Upload: $UPLOAD Ping: $PING ms Sleep: $TEST_INTERVAL $TIMESTAMP" | tee -a out.log
+	curl -s -i -XPOST 'http://$INFLUX_HOST:8086/write?db=bbk' --data-binary "ping,host=local value=$PING" > /dev/null
+	curl -s -i -XPOST 'http://$INFLUX_HOST:8086/write?db=bbk' --data-binary "download,host=local value=$DOWNLOAD" > /dev/null
+	curl -s -i -XPOST 'http://$INFLUX_HOST:8086/write?db=bbk' --data-binary "upload,host=local value=$UPLOAD" > /dev/null
+	sleep $TEST_INTERVAL
+
+	UPLOAD="-1"
+done
